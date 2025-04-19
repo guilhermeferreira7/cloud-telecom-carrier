0f2a61c8fc8f383cef92fd0c8d5ab2e9481a709d
guilhermeferreira7 <guilhermefribas@outlook.com>
2025-04-19 18:27:19 -0300

    update constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 /** Interval in milliseconds for updating data */
-export const DIDS_POLLING_INTERVAL = 1000 * 10; // 10 seconds
+export const DIDS_POLLING_INTERVAL = 1000 * 30; // 30 seconds
 
 /** Default limit for fetching */
-export const DIDS_DEFAULT_LIMIT = 5;
+export const DIDS_DEFAULT_LIMIT = 10;
