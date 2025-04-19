3f29ca87233bbd1e7eade56dd4852769ab4265cb
guilhermeferreira7 <guilhermefribas@outlook.com>
2025-04-19 10:58:14 -0300

    remove unused import

--- lib/features/dids/index.ts
+++ lib/features/dids/index.ts
@@ -1,5 +1,4 @@
 export * from "./didsSlice";
 export * from "./actions";
-export * from "./types";
 export * from "./components/DidsAvailableTable/DIDsAvailableTable";
 export * from "./components/DIDDetailsModal/DIDDetailsModal";
